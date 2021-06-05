5a73ade17894b88e7ebe37857524ca0813acb0f0
Hiroki-Ishimaru <maru.gooooooo@gmail.com>
2021-06-06 03:37:31 +0900

    Icons

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -14,51 +14,79 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child:  Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  width: 10,
-                  //color: Colors.blueGrey[100],
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/hiroid.jpg'),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      radius: 50.0,
-                      backgroundImage: AssetImage('images/hiroid.jpg'),
-                    ),
-                    Text(
-                      'HIROKI',
-                      style: TextStyle(
-                        fontFamily: 'Pacifico',
-                        fontSize: 25,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    Text(
-                      "FLUTTER BEGINNER",
-                      style: TextStyle(
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 20,
-                        color: Colors.teal.shade100,
-                        letterSpacing: 2,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Colors.blueGrey[100],
-                      child: Text("Container 1"),
-                    ),
-                  ],
+                Text(
+                  'HIROKI',
+                  style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                Text(
+                  "FLUTTER BEGINNER",
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
                 Container(
-                  width: 10,
-                  //color: Colors.blueGrey[100],
+                  color: Colors.white,
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        '+81 123 456 789',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.teal.shade500
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'example@example.com',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.teal.shade500
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
