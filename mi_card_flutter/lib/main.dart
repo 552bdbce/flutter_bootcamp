f7b06ec2cc36bd3b75340f2e12a9e76ff4482bb4
Hiroki-Ishimaru <maru.gooooooo@gmail.com>
2021-06-06 00:58:37 +0900

    column & row

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,3 +1,6 @@
+// ignore: avoid_web_libraries_in_flutter
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +13,35 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: 100,
+                  margin: EdgeInsets.only(left: 20),
+                  padding: EdgeInsets.all(20),
+                  color: Colors.blueGrey[100],
+                  child: Text("Container 1"),
+                ),
+                Container(
+                  height: 100,
+                  margin: EdgeInsets.only(left: 20),
+                  padding: EdgeInsets.all(20),
+                  color: Colors.blueGrey[100],
+                  child: Text("Container 1"),
+                ),
+                Container(
+                  height: 100,
+                  margin: EdgeInsets.only(left: 20),
+                  padding: EdgeInsets.all(20),
+                  color: Colors.blueGrey[100],
+                  child: Text("Container 1"),
+                ),
+              ],
+            ),
+        ),
       ),
     );
   }
