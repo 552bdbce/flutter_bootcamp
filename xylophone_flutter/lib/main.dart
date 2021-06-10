3a2af73a617722f30be8a09c379f535729818c06
Hiroki-Ishimaru <maru.gooooooo@gmail.com>
2021-06-10 13:25:30 +0900

    sound function

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -4,66 +4,67 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(soundNumber){
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Container(
-            child: Center(
-              child: Column(
-                children: [
-                  TextButton(
+            child: Column(
+              children: [
+                TextButton(
+                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.orange)),
+                  onPressed: (){
+                    playSound(1);
+                  },
+                ),
+                Container(
+                  color: Colors.pink,
+                  child: TextButton(
                     onPressed: (){
-                      final player = AudioCache();
-                      player.play('note1.wav');
+                      playSound(2);
                     },
                     child: Text(('Click me!')),
                   ),
-                  TextButton(
-                    onPressed: (){
-                      final player = AudioCache();
-                      player.play('note2.wav');
-                    },
-                    child: Text(('Click me!')),
-                  ),
-                  TextButton(
-                    onPressed: (){
-                      final player = AudioCache();
-                      player.play('note3.wav');
-                    },
-                    child: Text(('Click me!')),
-                  ),
-                  TextButton(
-                    onPressed: (){
-                      final player = AudioCache();
-                      player.play('note4.wav');
-                    },
-                    child: Text(('Click me!')),
-                  ),
-                  TextButton(
-                    onPressed: (){
-                      final player = AudioCache();
-                      player.play('note5.wav');
-                    },
-                    child: Text(('Click me!')),
-                  ),
-                  TextButton(
-                    onPressed: (){
-                      final player = AudioCache();
-                      player.play('note6.wav');
-                    },
-                    child: Text(('Click me!')),
-                  ),
-                  TextButton(
-                    onPressed: (){
-                      final player = AudioCache();
-                      player.play('note7.wav');
-                    },
-                    child: Text(('Click me!')),
-                  ),
-                ],
-              ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                  onPressed: (){
+                    playSound(3);
+                  },
+                  child: Text(('Click me!')),
+                ),
+                TextButton(
+                  onPressed: (){
+                    playSound(4);
+                  },
+                  child: Text(('Click me!')),
+                ),
+                TextButton(
+                  onPressed: (){
+                    playSound(5);
+                  },
+                  child: Text(('Click me!')),
+                ),
+                TextButton(
+                  onPressed: (){
+                    playSound(6);
+                  },
+                  child: Text(('Click me!')),
+                ),
+                TextButton(
+                  onPressed: (){
+                    playSound(7);
+                  },
+                  child: Text(('Click me!')),
+                ),
+              ],
             ),
           ),
         ),
