ab3c06da721c28c859b295bbcdcf2064a51cc39d
Hiroki-Ishimaru <maru.gooooooo@gmail.com>
2021-06-13 12:53:29 +0900

    no message

--- quizzler_flutter/lib/main.dart
+++ quizzler_flutter/lib/main.dart
@@ -29,14 +29,27 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  // List<String> questions = [
-  //   'You can lead a cow down stairs but not up stairs.',
-  //   'Approximately one quarter of human bones are in the feet.',
-  //   'A slug\'s blood is green.'
-  // ];
-  // List<bool> answers = [false, true, true];
 
-  int questionNumber = 0;
+  void checkAnswer(bool userPickedAnswer) {
+    setState(() {
+      if (quizBrain.getQuestionAnswer() == userPickedAnswer) {
+        scoreKeeper.add(
+          Icon(
+            Icons.check,
+            color: Colors.green,
+          ),
+        );
+      } else {
+        scoreKeeper.add(
+          Icon(
+            Icons.close,
+            color: Colors.red,
+          ),
+        );
+      }
+      quizBrain.nextQuestion();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -75,20 +88,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                if(quizBrain.getQuestionAnswer() == true){
-                  print('got it right');
-                } else {
-                  print('got wrong');
-                }
-                setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
-                  quizBrain.nextQuestion();
-                });
+                checkAnswer(true);
               },
             ),
           ),
@@ -106,20 +106,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                if(quizBrain.getQuestionAnswer() == false){
-                  print('got it right');
-                } else {
-                  print('got wrong');
-                }
-                setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ),
-                  );
-                  quizBrain.nextQuestion();
-                });
+                checkAnswer(false);
               },
             ),
           ),
