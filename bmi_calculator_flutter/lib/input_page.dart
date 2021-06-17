14330e438ff4db56088f54358983ae2e4490a9e3
Hiroki-Ishimaru <maru.gooooooo@gmail.com>
2021-06-18 00:02:05 +0900

    icon const

--- bmi_calculator_flutter/lib/input_page.dart
+++ bmi_calculator_flutter/lib/input_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -24,17 +25,42 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Expanded(child: ReusableCard(color: activeCardColor,)),
-                  Expanded(child: ReusableCard(color: activeCardColor,)),
+                  Expanded(
+                    child: ReusableCard(
+                      color: activeCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      color: activeCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'FEMALE',
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
-            Expanded(child: ReusableCard(color: activeCardColor,)),
+            Expanded(
+                child: ReusableCard(
+              color: activeCardColor,
+            )),
             Expanded(
               child: Row(
                 children: [
-                  Expanded(child: ReusableCard(color: activeCardColor,)),
-                  Expanded(child: ReusableCard(color: activeCardColor,)),
+                  Expanded(
+                      child: ReusableCard(
+                    color: activeCardColor,
+                  )),
+                  Expanded(
+                      child: ReusableCard(
+                    color: activeCardColor,
+                  )),
                 ],
               ),
             ),
@@ -51,13 +77,43 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  IconContent({this.icon, this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98)),
+        )
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
   final Color color;
-  ReusableCard({@required this.color});
+  final Widget cardChild;
+
+  ReusableCard({@required this.color, this.cardChild});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: color,
