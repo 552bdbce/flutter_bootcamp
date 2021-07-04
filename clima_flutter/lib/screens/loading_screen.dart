1bebcd94836ffea4590b6060ca5af1efd0042a9f
Hiroki-Ishimaru <maru.gooooooo@gmail.com>
2021-07-05 01:03:54 +0900

    weather api

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -28,8 +29,19 @@ class _LoadingScreenState extends State<LoadingScreen> {
       throw 'exception';
     }
   }
+  void getData()async{
+    http.Response response = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=2008d21525e04aea2aabc5e141f5eda1',));
+    if (response.statusCode == 200){
+      String data = response.body;
+      print(data);
+    }else{
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
 
     );
